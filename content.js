60ce6132e707ab25fbe4d6b30c8216c8a33c3d46
cobiliana <andrecob2@gmail.com>
2021-03-04 22:32:21 +0000

    simplifying + logo

--- content.js
+++ content.js
@@ -1,21 +1,9 @@
-window.addEventListener("load", () => {
+setInterval(() => {
     document.getElementsByClassName('piano-article-blocker')[0].remove()
 
-    setInterval(() => {
-        const articleBody = document.getElementsByClassName('article-body-wrapper')[0];
-        articleBody.style.maxHeight = '';
-        articleBody.style.overflow = '';
+    const articleBody = document.getElementsByClassName('article-body-wrapper')[0];
+    articleBody.style.maxHeight = '';
+    articleBody.style.overflow = '';
 
-        const blocks = articleBody.getElementsByClassName('article-block block-quote');
-        for (let i = 0; i < blocks.length; i++) {
-            blocks[i].style.opacity = '1 !important';
-            blocks[i].style.visibility = 'visible !important';
-        }
-
-        const bodies = document.getElementsByClassName('article-body fbg-container');
-        for (let i = 0; i < bodies.length; i++) {
-            bodies[i].style.opacity = '1 !important';
-            bodies[i].style.visibility = 'visible !important';
-        }
-    }, 2500);
-});
+    document.body.classList.remove('premium-article');
+}, 2500);
